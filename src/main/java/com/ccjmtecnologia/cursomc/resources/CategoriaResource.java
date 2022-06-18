f79b06babe99366f29156c6be7d37ae00b373bf8
Jose Maciel <josemaciel30@gmail.com>
2022-06-18 16:49:59 -0300

    Test first Domain Class 2022.06.18

--- src/main/java/com/ccjmtecnologia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ccjmtecnologia/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.ccjmtecnologia.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ccjmtecnologia.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST is working!";
+	public List<Categoria> list() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
