5a2536dd97aabc4bf34b40e96fa25730391daea5
Jose Maciel <josemaciel30@gmail.com>
2022-06-20 18:10:52 -0300

    States & Cities 2022.06.20

--- src/main/java/com/ccjmtecnologia/cursomc/CursomcApplication.java
+++ src/main/java/com/ccjmtecnologia/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ccjmtecnologia.cursomc.domain.Categoria;
+import com.ccjmtecnologia.cursomc.domain.Cidade;
+import com.ccjmtecnologia.cursomc.domain.Estado;
 import com.ccjmtecnologia.cursomc.domain.Produto;
 import com.ccjmtecnologia.cursomc.repositories.CategoriaRepository;
+import com.ccjmtecnologia.cursomc.repositories.CidadeRepository;
+import com.ccjmtecnologia.cursomc.repositories.EstadoRepository;
 import com.ccjmtecnologia.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -33,7 +41,7 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-
+		
 		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
 
@@ -43,6 +51,21 @@ public class CursomcApplication implements CommandLineRunner{
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCities().addAll(Arrays.asList(c1));
+		est2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+	
+		
 	}
 
 }
---
+++ src/main/java/com/ccjmtecnologia/cursomc/domain/Cidade.java
@@ -0,0 +1,70 @@
+package com.ccjmtecnologia.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cidade  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "estado_id")
+	private Estado state;
+	public Cidade() {
+		
+	}
+	public Cidade(Integer id, String name, Estado state) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.state = state;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Estado getState() {
+		return state;
+	}
+	public void setState(Estado state) {
+		this.state = state;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
