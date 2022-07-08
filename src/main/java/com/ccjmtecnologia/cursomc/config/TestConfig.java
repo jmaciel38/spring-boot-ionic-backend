ee4ded359b27115f95f9e6124202c2990e7f3f22
Jose Maciel <josemaciel30@gmail.com>
2022-07-08 09:49:01 -0300

    Create Test Profile 2022.07.08

---
+++ src/main/java/com/ccjmtecnologia/cursomc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.ccjmtecnologia.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ccjmtecnologia.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	@Bean
+	boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
